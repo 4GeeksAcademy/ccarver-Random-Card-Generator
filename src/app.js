70a1f9f334beec9fbd8cb0c5dbf52bfc75275e14
Seeker90 <chris.carver90@icloud.com>
2025-08-28 00:19:39 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+function getRandomCard() {
+  let suits = ["Hearts","Spades","Clubs","Diamond"];
+  let values = ["2", "3", "4", "5", "6", "7", "8" "9", "10", "J", "Q" "K", "A"];
+
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+  let value = values[Math.floor(Math.random() * values.length)]
+  
+  return {suit, value };
+  
+  
+  
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
